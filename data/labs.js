a413dd6246a329582127f8475ef6abd132f181f6
krazykilla33 <jonathan3337@gmail.com>
2026-07-05 14:43:46 -0400

    Update labs.js

--- data/labs.js
+++ data/labs.js
@@ -1,330 +1,617 @@
 window.LABS = [
   {
-    id: "bee-lab",
-    name: "Bee Lab",
-    tables: { coke: 3, crack: 2, plant: 2 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "bee-lab",
+    "name": "Bee Lab",
+    "tables": {
+      "coke": 3,
+      "crack": 2,
+      "plant": 2
+    },
+    "map": {
+      "x": 525,
+      "y": 386
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "rockford",
-    name: "Rockford",
-    tables: { coke: 4, crack: 0, plant: 0 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "rockford",
+    "name": "Rockford",
+    "tables": {
+      "coke": 4,
+      "crack": 0,
+      "plant": 0
+    },
+    "map": {
+      "x": null,
+      "y": null
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "los-pollos-cluckin",
-    name: "Los Pollos (Cluckin)",
-    tables: { coke: 0, crack: 0, plant: 0 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "los-pollos-cluckin",
+    "name": "Los Pollos (Cluckin)",
+    "tables": {
+      "coke": 0,
+      "crack": 0,
+      "plant": 0
+    },
+    "map": {
+      "x": null,
+      "y": null
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "flower-vespucci",
-    name: "Flower (Vespucci)",
-    tables: { coke: 4, crack: 4, plant: 2 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "flower-vespucci",
+    "name": "Flower (Vespucci)",
+    "tables": {
+      "coke": 4,
+      "crack": 4,
+      "plant": 2
+    },
+    "map": {
+      "x": 465,
+      "y": 294
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "sewer",
-    name: "Sewer",
-    tables: { coke: 6, crack: 5, plant: 0 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "sewer",
+    "name": "Sewer",
+    "tables": {
+      "coke": 6,
+      "crack": 5,
+      "plant": 0
+    },
+    "map": {
+      "x": 535,
+      "y": 268
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "city-scrapyard",
-    name: "city Scrapyard",
-    tables: { coke: 5, crack: 3, plant: 3 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "city-scrapyard",
+    "name": "city Scrapyard",
+    "tables": {
+      "coke": 5,
+      "crack": 3,
+      "plant": 3
+    },
+    "map": {
+      "x": 523,
+      "y": 246
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "voodoo-banning",
-    name: "Voodoo (Banning)",
-    tables: { coke: 8, crack: 3, plant: 0 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "voodoo-banning",
+    "name": "Voodoo (Banning)",
+    "tables": {
+      "coke": 8,
+      "crack": 3,
+      "plant": 0
+    },
+    "map": {
+      "x": 576,
+      "y": 187
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "elysian",
-    name: "Elysian",
-    tables: { coke: 4, crack: 6, plant: 3 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "elysian",
+    "name": "Elysian",
+    "tables": {
+      "coke": 4,
+      "crack": 6,
+      "plant": 3
+    },
+    "map": {
+      "x": 590,
+      "y": 142
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "merryweather",
-    name: "Merryweather",
-    tables: { coke: 5, crack: 5, plant: 6 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "merryweather",
+    "name": "Merryweather",
+    "tables": {
+      "coke": 5,
+      "crack": 5,
+      "plant": 6
+    },
+    "map": {
+      "x": 649,
+      "y": 101
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "container-2-import",
-    name: "Container 2 (Import)",
-    tables: { coke: 5, crack: 5, plant: 3 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "container-2-import",
+    "name": "Container 2 (Import)",
+    "tables": {
+      "coke": 5,
+      "crack": 5,
+      "plant": 3
+    },
+    "map": {
+      "x": 691,
+      "y": 103
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "container-1-arena",
-    name: "Container 1 (Arena)",
-    tables: { coke: 4, crack: 4, plant: 3 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "container-1-arena",
+    "name": "Container 1 (Arena)",
+    "tables": {
+      "coke": 4,
+      "crack": 4,
+      "plant": 3
+    },
+    "map": {
+      "x": 697,
+      "y": 163
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "garbage",
-    name: "Garbage",
-    tables: { coke: 8, crack: 8, plant: 2 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "garbage",
+    "name": "Garbage",
+    "tables": {
+      "coke": 8,
+      "crack": 8,
+      "plant": 2
+    },
+    "map": {
+      "x": 742,
+      "y": 196
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "underpass",
-    name: "Underpass",
-    tables: { coke: 3, crack: 3, plant: 2 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "underpass",
+    "name": "Underpass",
+    "tables": {
+      "coke": 3,
+      "crack": 3,
+      "plant": 2
+    },
+    "map": {
+      "x": null,
+      "y": null
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "dam",
-    name: "Dam",
-    tables: { coke: 4, crack: 2, plant: 3 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "dam",
+    "name": "Dam",
+    "tables": {
+      "coke": 4,
+      "crack": 2,
+      "plant": 3
+    },
+    "map": {
+      "x": 782,
+      "y": 448
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "island",
-    name: "Island",
-    tables: { coke: 8, crack: 4, plant: 5 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "island",
+    "name": "Island",
+    "tables": {
+      "coke": 8,
+      "crack": 4,
+      "plant": 5
+    },
+    "map": {
+      "x": 879,
+      "y": 268
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "lfb-senora-warehouses",
-    name: "LFB (Senora Warehouses)",
-    tables: { coke: 4, crack: 8, plant: 2 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "lfb-senora-warehouses",
+    "name": "LFB (Senora Warehouses)",
+    "tables": {
+      "coke": 4,
+      "crack": 8,
+      "plant": 2
+    },
+    "map": {
+      "x": 712,
+      "y": 612
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "outhouse",
-    name: "Outhouse",
-    tables: { coke: 0, crack: 0, plant: 0 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "outhouse",
+    "name": "Outhouse",
+    "tables": {
+      "coke": 0,
+      "crack": 0,
+      "plant": 0
+    },
+    "map": {
+      "x": null,
+      "y": null
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "sandy-lsd",
-    name: "Sandy (LSD)",
-    tables: { coke: 8, crack: 2, plant: 1 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "sandy-lsd",
+    "name": "Sandy (LSD)",
+    "tables": {
+      "coke": 8,
+      "crack": 2,
+      "plant": 1
+    },
+    "map": {
+      "x": 730,
+      "y": 788
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "sea-labs",
-    name: "Sea Labs",
-    tables: { coke: 8, crack: 4, plant: 3 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "sea-labs",
+    "name": "Sea Labs",
+    "tables": {
+      "coke": 8,
+      "crack": 4,
+      "plant": 3
+    },
+    "map": {
+      "x": 980,
+      "y": 875
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "grandmas",
-    name: "Grandmas",
-    tables: { coke: 4, crack: 4, plant: 3 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "grandmas",
+    "name": "Grandmas",
+    "tables": {
+      "coke": 4,
+      "crack": 4,
+      "plant": 3
+    },
+    "map": {
+      "x": 846,
+      "y": 928
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "braddocks",
-    name: "Braddocks",
-    tables: { coke: 9, crack: 5, plant: 1 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "braddocks",
+    "name": "Braddocks",
+    "tables": {
+      "coke": 9,
+      "crack": 5,
+      "plant": 1
+    },
+    "map": {
+      "x": 813,
+      "y": 994
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "north-lab-dignity",
-    name: "North Lab (Dignity)",
-    tables: { coke: 6, crack: 6, plant: 3 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "north-lab-dignity",
+    "name": "North Lab (Dignity)",
+    "tables": {
+      "coke": 6,
+      "crack": 6,
+      "plant": 3
+    },
+    "map": {
+      "x": 746,
+      "y": 1068
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "quarry",
-    name: "Quarry",
-    tables: { coke: 5, crack: 3, plant: 1 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "quarry",
+    "name": "Quarry",
+    "tables": {
+      "coke": 5,
+      "crack": 3,
+      "plant": 1
+    },
+    "map": {
+      "x": 890,
+      "y": 703
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "pirate-bay-cove",
-    name: "Pirate Bay (cove)",
-    tables: { coke: 8, crack: 5, plant: 2 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "pirate-bay-cove",
+    "name": "Pirate Bay (cove)",
+    "tables": {
+      "coke": 8,
+      "crack": 5,
+      "plant": 2
+    },
+    "map": {
+      "x": 904,
+      "y": 638
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "airplane-scrap",
-    name: "Airplane Scrap",
-    tables: { coke: 5, crack: 2, plant: 2 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "airplane-scrap",
+    "name": "Airplane Scrap",
+    "tables": {
+      "coke": 5,
+      "crack": 2,
+      "plant": 2
+    },
+    "map": {
+      "x": 837,
+      "y": 734
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "chili",
-    name: "CHILI",
-    tables: { coke: 0, crack: 6, plant: 1 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "chili",
+    "name": "CHILI",
+    "tables": {
+      "coke": 0,
+      "crack": 6,
+      "plant": 1
+    },
+    "map": {
+      "x": 469,
+      "y": 925
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "galilee-right-lake",
-    name: "Galilee (Right Lake)",
-    tables: { coke: 0, crack: 6, plant: 1 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "galilee-right-lake",
+    "name": "Galilee (Right Lake)",
+    "tables": {
+      "coke": 0,
+      "crack": 6,
+      "plant": 1
+    },
+    "map": {
+      "x": 725,
+      "y": 868
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "tug-boat",
-    name: "Tug Boat",
-    tables: { coke: 3, crack: 3, plant: 3 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "tug-boat",
+    "name": "Tug Boat",
+    "tables": {
+      "coke": 3,
+      "crack": 3,
+      "plant": 3
+    },
+    "map": {
+      "x": 426,
+      "y": 951
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "cassidy-airplane-crash",
-    name: "Cassidy (Airplane crash)",
-    tables: { coke: 5, crack: 1, plant: 2 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "cassidy-airplane-crash",
+    "name": "Cassidy (Airplane crash)",
+    "tables": {
+      "coke": 5,
+      "crack": 1,
+      "plant": 2
+    },
+    "map": {
+      "x": 447,
+      "y": 848
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "shipment",
-    name: "Shipment",
-    tables: { coke: 5, crack: 4, plant: 3 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "shipment",
+    "name": "Shipment",
+    "tables": {
+      "coke": 5,
+      "crack": 4,
+      "plant": 3
+    },
+    "map": {
+      "x": 683,
+      "y": 120
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "afghan",
-    name: "Afghan",
-    tables: { coke: 8, crack: 8, plant: 2 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "afghan",
+    "name": "Afghan",
+    "tables": {
+      "coke": 8,
+      "crack": 8,
+      "plant": 2
+    },
+    "map": {
+      "x": 683,
+      "y": 728
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "noose",
-    name: "NOOSE",
-    tables: { coke: 5, crack: 3, plant: 2 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "noose",
+    "name": "NOOSE",
+    "tables": {
+      "coke": 5,
+      "crack": 3,
+      "plant": 2
+    },
+    "map": {
+      "x": 847,
+      "y": 397
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "cayo",
-    name: "Cayo",
-    tables: { coke: 11, crack: 9, plant: 0 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "cayo",
+    "name": "Cayo",
+    "tables": {
+      "coke": 11,
+      "crack": 9,
+      "plant": 0
+    },
+    "map": {
+      "x": null,
+      "y": null
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "rust",
-    name: "Rust",
-    tables: { coke: 8, crack: 8, plant: 1 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "rust",
+    "name": "Rust",
+    "tables": {
+      "coke": 8,
+      "crack": 8,
+      "plant": 1
+    },
+    "map": {
+      "x": null,
+      "y": null
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "vinewood-sign",
-    name: "Vinewood Sign",
-    tables: { coke: 8, crack: 3, plant: 1 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "vinewood-sign",
+    "name": "Vinewood Sign",
+    "tables": {
+      "coke": 8,
+      "crack": 3,
+      "plant": 1
+    },
+    "map": {
+      "x": 663,
+      "y": 549
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "paleto-train",
-    name: "Paleto Train",
-    tables: { coke: 8, crack: 6, plant: 2 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "paleto-train",
+    "name": "Paleto Train",
+    "tables": {
+      "coke": 8,
+      "crack": 6,
+      "plant": 2
+    },
+    "map": {
+      "x": 568,
+      "y": 1040
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "orange-ball-observatory",
-    name: "Orange Ball (Observatory)",
-    tables: { coke: 8, crack: 6, plant: 1 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "orange-ball-observatory",
+    "name": "Orange Ball (Observatory)",
+    "tables": {
+      "coke": 8,
+      "crack": 6,
+      "plant": 1
+    },
+    "map": {
+      "x": 543,
+      "y": 579
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "mission-row-train",
-    name: "Mission Row Train",
-    tables: { coke: 8, crack: 8, plant: 0 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "mission-row-train",
+    "name": "Mission Row Train",
+    "tables": {
+      "coke": 8,
+      "crack": 8,
+      "plant": 0
+    },
+    "map": {
+      "x": 649,
+      "y": 371
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "zancudo",
-    name: "Zancudo",
-    tables: { coke: 8, crack: 8, plant: 2 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "zancudo",
+    "name": "Zancudo",
+    "tables": {
+      "coke": 8,
+      "crack": 8,
+      "plant": 2
+    },
+    "map": {
+      "x": 422,
+      "y": 734
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "cement",
-    name: "Cement",
-    tables: { coke: 5, crack: 8, plant: 1 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "cement",
+    "name": "Cement",
+    "tables": {
+      "coke": 5,
+      "crack": 8,
+      "plant": 1
+    },
+    "map": {
+      "x": 618,
+      "y": 714
+    },
+    "status": "unknown",
+    "notes": ""
   },
   {
-    id: "juans",
-    name: "Juans",
-    tables: { coke: 8, crack: 8, plant: 3 },
-    map: { x: null, y: null },
-    status: "unknown",
-    notes: ""
+    "id": "juans",
+    "name": "Juans",
+    "tables": {
+      "coke": 8,
+      "crack": 8,
+      "plant": 3
+    },
+    "map": {
+      "x": 683,
+      "y": 263
+    },
+    "status": "unknown",
+    "notes": ""
   }
 ];
