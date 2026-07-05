3c04ac61f7826014cc7627c4b70785c210df7afb
krazykilla33 <jonathan3337@gmail.com>
2026-07-05 15:10:24 -0400

    Update lab-map.js

--- lab-map.js
+++ lab-map.js
@@ -22,7 +22,7 @@ L.imageOverlay(mapImage, bounds).addTo(map);
 map.fitBounds(bounds);
 
 // Starts only slightly closer than full view
-map.setZoom(-0.25);
+map.setZoom(-0.75);
 
 const markerLayer = L.layerGroup().addTo(map);
 const markerMap = {};
